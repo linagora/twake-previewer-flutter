79b264694fc96ebe331077d1efe6d6a02c023567
DatDang <tddang@linagora.com>
2025-02-28 17:31:30 +0700

    Image: Apply template and Esc key listener

--- lib/twake_image_previewer/twake_image_previewer.dart
+++ lib/twake_image_previewer/twake_image_previewer.dart
@@ -1,16 +1,20 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:twake_previewer_flutter/core/previewer_options/options/loading_options.dart';
 import 'package:twake_previewer_flutter/core/previewer_options/options/top_bar_options.dart';
 import 'package:twake_previewer_flutter/core/previewer_options/previewer_options.dart';
+import 'package:twake_previewer_flutter/core/utils/utils.dart';
+import 'package:twake_previewer_flutter/core/widgets/previewer_template_widget.dart';
 import 'package:twake_previewer_flutter/core/widgets/top_bar_widget.dart';
 
-class TwakeImagePreviewer extends StatelessWidget {
+class TwakeImagePreviewer extends StatefulWidget {
   const TwakeImagePreviewer({
     super.key,
     this.bytes,
     this.errorBuilder,
     this.previewerOptions,
+    this.loadingOptions,
     this.topBarOptions,
     this.zoomable = false,
   });
@@ -18,47 +22,84 @@ class TwakeImagePreviewer extends StatelessWidget {
   final Uint8List? bytes;
   final Widget Function(BuildContext, Object, StackTrace?)? errorBuilder;
   final PreviewerOptions? previewerOptions;
+  final LoadingOptions? loadingOptions;
   final TopBarOptions? topBarOptions;
   final bool zoomable;
 
   @override
-  Widget build(BuildContext context) {
-    Widget previewerChild = bytes == null
-        ? const SizedBox()
-        : Image.memory(
-            bytes!,
-            width: previewerOptions?.width,
-            height: previewerOptions?.height,
-            errorBuilder: errorBuilder,
-          );
+  State<TwakeImagePreviewer> createState() => _TwakeImagePreviewerState();
+}
 
-    if (zoomable) {
-      previewerChild = InteractiveViewer(
-        child: Center(
-          child: previewerChild,
+class _TwakeImagePreviewerState extends State<TwakeImagePreviewer> {
+  final _focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget previewerChild = PreviewerTemplateWidget(
+      previewerOptions: widget.previewerOptions,
+      loadingOptions: widget.loadingOptions,
+      child: widget.bytes == null
+          ? const SizedBox()
+          : Image.memory(
+              widget.bytes!,
+              width: widget.previewerOptions?.width,
+              height: widget.previewerOptions?.height,
+              errorBuilder: widget.errorBuilder,
+            ),
+    );
+
+    if (widget.zoomable) {
+      previewerChild = GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          Navigator.maybePop(context);
+        },
+        child: InteractiveViewer(
+          child: Center(
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                // If this onTap is null, the parent GestureDetector's onTap will be triggered
+              },
+              child: previewerChild,
+            ),
+          ),
         ),
       );
     }
 
-    final topBarChild = topBarOptions == null
+    final topBarChild = widget.topBarOptions == null
         ? const SizedBox()
         : TopBarWidget(
-            title: topBarOptions!.title,
-            closeAction: topBarOptions!.onClose,
-            closeTooltip: topBarOptions!.closeTooltip,
-            downloadAction: topBarOptions!.onDownload,
-            downloadTooltip: topBarOptions!.downloadTooltip,
-            printAction: topBarOptions!.onPrint,
-            printTooltip: topBarOptions!.printTooltip,
+            title: widget.topBarOptions!.title,
+            closeAction: widget.topBarOptions!.onClose,
+            closeTooltip: widget.topBarOptions!.closeTooltip,
+            downloadAction: widget.topBarOptions!.onDownload,
+            downloadTooltip: widget.topBarOptions!.downloadTooltip,
+            printAction: widget.topBarOptions!.onPrint,
+            printTooltip: widget.topBarOptions!.printTooltip,
           );
 
-    return Column(
-      children: [
-        topBarChild,
-        Expanded(
-          child: previewerChild,
-        ),
-      ],
+    return KeyboardListener(
+      focusNode: _focusNode,
+      autofocus: true,
+      onKeyEvent: (value) {
+        Utils.handleEscapeKey(value, widget.topBarOptions?.onClose);
+      },
+      child: Column(
+        children: [
+          topBarChild,
+          Expanded(
+            child: previewerChild,
+          ),
+        ],
+      ),
     );
   }
 }
